5de607ed540e68579a65cbaf7a14ee476e09549f
Tomer <42910634+tomer-friedman@users.noreply.github.com>
2023-12-07 10:23:46 -0800

    feat: filter nextjs (#20)

---
+++ packages/traceloop-sdk/src/lib/tracing/sampler.ts
@@ -0,0 +1,36 @@
+import {
+  Sampler,
+  SamplingResult,
+  SamplingDecision,
+} from "@opentelemetry/sdk-trace-base";
+import { Context, SpanKind, Attributes, Link } from "@opentelemetry/api";
+
+const FILTERED_ATTRIBUTE_KEYS = ["next.span_name"];
+
+export class TraceloopSampler implements Sampler {
+  shouldSample(
+    _context: Context,
+    _traceId: string,
+    _spanName: string,
+    _spanKind: SpanKind,
+    attributes: Attributes,
+    _links: Link[],
+  ): SamplingResult {
+    let filter = false;
+    FILTERED_ATTRIBUTE_KEYS.forEach((key) => {
+      if (attributes?.[key]) {
+        filter = true;
+      }
+    });
+
+    return {
+      decision: filter
+        ? SamplingDecision.NOT_RECORD
+        : SamplingDecision.RECORD_AND_SAMPLED,
+    };
+  }
+
+  toString(): string {
+    return "TraceloopSampler";
+  }
+}
