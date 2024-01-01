eeeda7824a82449a9cc94da05cf6c751c2b3b4cc
Kartik Prajapati <kartikprajapati987@gmail.com>
2024-01-01 19:34:04 +0530

    Complete instrumentation

--- packages/sample-app/src/sample_llamaindex.ts
+++ packages/sample-app/src/sample_llamaindex.ts
@@ -7,13 +7,11 @@ import {
   serviceContextFromDefaults,
   storageContextFromDefaults,
 } from "llamaindex";
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
 
 traceloop.initialize({
   appName: "sample_llamaindex",
   apiKey: process.env.TRACELOOP_API_KEY,
   disableBatch: true,
-  exporter: new ConsoleSpanExporter(),
 });
 
 const embedModel = new OpenAIEmbedding();
@@ -22,41 +20,28 @@ const vectorStore = new PGVectorStore();
 
 vectorStore.setCollection('quickstart');
 
-(async () => {
-  const documents = await (new SimpleDirectoryReader()).loadData({ directoryPath: '/Users/k/personal/openllmetry-js/packages/sample-app/src/data/' });
+class SampleLlamaIndex {
+  @traceloop.workflow("sample_query")
+  async query() {
+    const documents = await (new SimpleDirectoryReader()).loadData({ directoryPath: 'packages/sample-app/data/paul_graham' });
 
-  const serviceContext = serviceContextFromDefaults({ embedModel });
-  const storageContext = await storageContextFromDefaults({ vectorStore });
+    const serviceContext = serviceContextFromDefaults({ embedModel });
+    const storageContext = await storageContextFromDefaults({ vectorStore });
 
-  const index = await VectorStoreIndex.fromDocuments(documents, { storageContext, serviceContext });
+    const index = await VectorStoreIndex.fromDocuments(documents, { storageContext, serviceContext });
 
-  const queryEngine = index.asQueryEngine();
+    const queryEngine = index.asQueryEngine();
 
-  const res = await queryEngine.query('What did the author do growing up?');
+    const res = await queryEngine.query('What did the author do growing up?');
+    return res
+  }
+}
 
-  console.log(res);
-})()
-  .then(() => process.exit(0))
-  .catch((e) => {console.log(e); process.exit(1);});
-
-// const openaiLLM = new OpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
-//class SampleLlamaIndex {
-//  @traceloop.workflow("sample_completion")
-//  async completion() {
-//    const resp = await openaiLLM.complete("What's you name?");
-//    return resp;
-//  }
-//
-//}
-//
-//traceloop.withAssociationProperties(
-//  { user_id: "12345", chat_id: "789" },
-//  async () => {
-//    const sampleLlamaIndex = new SampleLlamaIndex();
-//
-//    const completion = await sampleLlamaIndex.completion();
-//
-//    console.log('completion', completion);
-//  },
-//);
-//
+traceloop.withAssociationProperties(
+  { user_id: "12345", chat_id: "789" },
+  async () => {
+    const sampleLlamaIndex = new SampleLlamaIndex();
+    const result = await sampleLlamaIndex.query();
+    console.log(result.response);
+  },
+);
