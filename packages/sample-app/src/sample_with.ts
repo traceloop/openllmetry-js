c524ee0d6dc2189bbb4d496047f40622e86981e2
Tomer <4tomer.friedman@gmail.com>
2024-01-08 10:00:42 +0200

    promise all

--- packages/sample-app/src/sample_with.ts
+++ packages/sample-app/src/sample_with.ts
@@ -1,40 +1,60 @@
 import * as traceloop from "@traceloop/node-server-sdk";
 import OpenAI from "openai";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 
 traceloop.initialize({
   appName: "sample_openai",
   apiKey: process.env.TRACELOOP_API_KEY,
   disableBatch: true,
+  exporter: new ConsoleSpanExporter(),
 });
 const openai = new OpenAI();
 
-async function chat() {
-  return await traceloop.withWorkflow("sample_chat", async () => {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        { role: "user", content: "Tell me a joke about OpenTelemetry" },
-      ],
-      model: "gpt-3.5-turbo",
-    });
+// async function chat() {
+//   return await traceloop.withWorkflow("sample_chat", async () => {
+//     const chatCompletion = await openai.chat.completions.create({
+//       messages: [
+//         { role: "user", content: "Tell me a joke about OpenTelemetry" },
+//       ],
+//       model: "gpt-3.5-turbo",
+//     });
 
-    return chatCompletion.choices[0].message.content;
-  });
-}
+//     return chatCompletion.choices[0].message.content;
+//   });
+// }
 
 async function completion() {
-  return await traceloop.withWorkflow("sample_completion", async () => {
-    const completion = await openai.completions.create({
-      prompt: "Tell me a joke about TypeScript",
-      model: "gpt-3.5-turbo-instruct",
-    });
+  return await Promise.all([
+    traceloop.withWorkflow("sample_completion11111111", async () => {
+      const completion = await openai.completions.create({
+        prompt: "Tell me a joke about TypeScript",
+        model: "gpt-3.5-turbo-instruct",
+      });
 
-    return completion.choices[0].text;
-  });
+      return completion.choices[0].text;
+    }),
+    traceloop.withWorkflow("sample_completion2222222", async () => {
+      const completion = await openai.completions.create({
+        prompt: "Tell me a joke about Python",
+        model: "gpt-3.5-turbo-instruct",
+      });
+
+      return completion.choices[0].text;
+    }),
+    traceloop.withWorkflow("sample_completion3333333", async () => {
+      const completion = await openai.completions.create({
+        prompt: "Tell me a joke about Java",
+        model: "gpt-3.5-turbo-instruct",
+      });
+
+      return completion.choices[0].text;
+    }),
+  ]);
 }
 
 traceloop.withAssociationProperties({ userId: "12345" }, async () => {
-  const chatResponse = await chat();
-  console.log(chatResponse);
+  // const chatResponse = await chat();
+  // console.log(chatResponse);
 
   const completionResponse = await completion();
   console.log(completionResponse);
