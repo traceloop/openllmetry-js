20222736ec887b35073d0240083a0cc2e675e130
Nir Gazit <nirga@users.noreply.github.com>
2024-02-06 12:32:59 +0200

    chore: temporarily disabled new instrumentations

--- packages/traceloop-sdk/src/lib/tracing/index.ts
+++ packages/traceloop-sdk/src/lib/tracing/index.ts
@@ -10,15 +10,6 @@ import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions"
 import { Instrumentation } from "@opentelemetry/instrumentation";
 import { InitializeOptions } from "../interfaces";
 import { OpenAIInstrumentation } from "@traceloop/instrumentation-openai";
-import { LlamaIndexInstrumentation } from "@traceloop/instrumentation-llamaindex";
-import { PineconeInstrumentation } from "@traceloop/instrumentation-pinecone";
-import {
-  VertexAIInstrumentation,
-  AIPlatformInstrumentation,
-} from "@traceloop/instrumentation-vertexai";
-import { LangChainInstrumentation } from "@traceloop/instrumentation-langchain";
-import { BedrockInstrumentation } from "@traceloop/instrumentation-bedrock";
-import { CohereInstrumentation } from "@traceloop/instrumentation-cohere";
 import { SpanAttributes } from "@traceloop/ai-semantic-conventions";
 import { ASSOCATION_PROPERTIES_KEY, WORKFLOW_NAME_KEY } from "./tracing";
 import { Telemetry } from "../telemetry/telemetry";
@@ -28,13 +19,12 @@ import { _configuration } from "../configuration";
 let _sdk: NodeSDK;
 let _spanProcessor: SimpleSpanProcessor | BatchSpanProcessor;
 let openAIInstrumentation: OpenAIInstrumentation;
-let llamaIndexInstrumentation: LlamaIndexInstrumentation;
-let pineconeInstrumentation: PineconeInstrumentation;
-let vertexaiInstrumentation: VertexAIInstrumentation;
-let aiplatformInstrumentation: AIPlatformInstrumentation;
-let langChainInstrumentation: LangChainInstrumentation;
-let bedrockInstrumentation: BedrockInstrumentation;
-let cohereInstrumentation: CohereInstrumentation;
+// let llamaIndexInstrumentation: LlamaIndexInstrumentation;
+// let pineconeInstrumentation: PineconeInstrumentation;
+// let vertexaiInstrumentation: VertexAIInstrumentation;
+// let aiplatformInstrumentation: AIPlatformInstrumentation;
+// let langChainInstrumentation: LangChainInstrumentation;
+// let bedrockInstrumentation: BedrockInstrumentation;
 
 const instrumentations: Instrumentation[] = [];
 
@@ -42,26 +32,23 @@ export const initInstrumentations = () => {
   openAIInstrumentation = new OpenAIInstrumentation();
   instrumentations.push(openAIInstrumentation);
 
-  llamaIndexInstrumentation = new LlamaIndexInstrumentation();
-  instrumentations.push(llamaIndexInstrumentation);
+  // llamaIndexInstrumentation = new LlamaIndexInstrumentation();
+  // instrumentations.push(llamaIndexInstrumentation);
 
-  pineconeInstrumentation = new PineconeInstrumentation();
-  instrumentations.push(pineconeInstrumentation);
+  // pineconeInstrumentation = new PineconeInstrumentation();
+  // instrumentations.push(pineconeInstrumentation);
 
-  vertexaiInstrumentation = new VertexAIInstrumentation();
-  instrumentations.push(vertexaiInstrumentation);
+  // vertexaiInstrumentation = new VertexAIInstrumentation();
+  // instrumentations.push(vertexaiInstrumentation);
 
-  aiplatformInstrumentation = new AIPlatformInstrumentation();
-  instrumentations.push(aiplatformInstrumentation);
+  // aiplatformInstrumentation = new AIPlatformInstrumentation();
+  // instrumentations.push(aiplatformInstrumentation);
 
-  langChainInstrumentation = new LangChainInstrumentation();
-  instrumentations.push(openAIInstrumentation, langChainInstrumentation);
+  // langChainInstrumentation = new LangChainInstrumentation();
+  // instrumentations.push(openAIInstrumentation, langChainInstrumentation);
 
-  bedrockInstrumentation = new BedrockInstrumentation();
-  instrumentations.push(bedrockInstrumentation);
-
-  cohereInstrumentation = new CohereInstrumentation();
-  instrumentations.push(cohereInstrumentation);
+  // bedrockInstrumentation = new BedrockInstrumentation();
+  // instrumentations.push(bedrockInstrumentation);
 };
 
 /**
@@ -74,11 +61,10 @@ export const initInstrumentations = () => {
 export const startTracing = (options: InitializeOptions) => {
   if (!shouldSendTraces()) {
     openAIInstrumentation.setConfig({ traceContent: false });
-    llamaIndexInstrumentation.setConfig({ traceContent: false });
-    vertexaiInstrumentation.setConfig({ traceContent: false });
-    aiplatformInstrumentation.setConfig({ traceContent: false });
-    bedrockInstrumentation.setConfig({ traceContent: false });
-    cohereInstrumentation.setConfig({ traceContent: false });
+    // llamaIndexInstrumentation.setConfig({ traceContent: false });
+    // vertexaiInstrumentation.setConfig({ traceContent: false });
+    // aiplatformInstrumentation.setConfig({ traceContent: false });
+    // bedrockInstrumentation.setConfig({ traceContent: false });
   }
 
   const traceExporter =
@@ -140,37 +126,33 @@ export const startTracing = (options: InitializeOptions) => {
   if (options.instrumentModules?.openAI) {
     openAIInstrumentation.manuallyInstrument(options.instrumentModules.openAI);
   }
-  if (options.instrumentModules?.llamaIndex) {
-    llamaIndexInstrumentation.manuallyInstrument(
-      options.instrumentModules.llamaIndex,
-    );
-  }
-  if (options.instrumentModules?.pinecone) {
-    pineconeInstrumentation.manuallyInstrument(
-      options.instrumentModules.pinecone,
-    );
-  }
-  if (options.instrumentModules?.google_vertexai) {
-    vertexaiInstrumentation.manuallyInstrument(
-      options.instrumentModules.google_vertexai,
-    );
-  }
+  // if (options.instrumentModules?.llamaIndex) {
+  //   llamaIndexInstrumentation.manuallyInstrument(
+  //     options.instrumentModules.llamaIndex,
+  //   );
+  // }
+  // if (options.instrumentModules?.pinecone) {
+  //   pineconeInstrumentation.manuallyInstrument(
+  //     options.instrumentModules.pinecone,
+  //   );
+  // }
+  // if (options.instrumentModules?.google_vertexai) {
+  //   vertexaiInstrumentation.manuallyInstrument(
+  //     options.instrumentModules.google_vertexai,
+  //   );
+  // }
 
-  if (options.instrumentModules?.google_aiplatform) {
-    aiplatformInstrumentation.manuallyInstrument(
-      options.instrumentModules.google_aiplatform,
-    );
-  }
+  // if (options.instrumentModules?.google_aiplatform) {
+  //   aiplatformInstrumentation.manuallyInstrument(
+  //     options.instrumentModules.google_aiplatform,
+  //   );
+  // }
 
-  if (options.instrumentModules?.bedrock) {
-    bedrockInstrumentation.manuallyInstrument(
-      options.instrumentModules.bedrock,
-    );
-  }
-
-  if (options.instrumentModules?.cohere) {
-    cohereInstrumentation.manuallyInstrument(options.instrumentModules.cohere);
-  }
+  // if (options.instrumentModules?.bedrock) {
+  //   bedrockInstrumentation.manuallyInstrument(
+  //     options.instrumentModules.bedrock,
+  //   );
+  // }
 };
 
 export const shouldSendTraces = () => {
