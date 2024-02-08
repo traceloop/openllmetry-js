4b2df5779d2d663569152e69b817e8c7ba8f7016
5war00p <maddisaiswaroop@gmail.com>
2024-02-08 11:44:33 +0530

    partial instrumentation

--- packages/instrumentation-cohere/src/instrumentation.ts
+++ packages/instrumentation-cohere/src/instrumentation.ts
@@ -14,18 +14,31 @@
  * limitations under the License.
  */
 import {
+  Span,
+  Attributes,
+  SpanKind,
+  SpanStatusCode,
+  context,
+  trace,
+} from "@opentelemetry/api";
+import {
   InstrumentationBase,
   InstrumentationModuleDefinition,
   InstrumentationNodeModuleDefinition,
+  safeExecuteInTheMiddle,
 } from "@opentelemetry/instrumentation";
 import { CohereInstrumentationConfig } from "./types";
 import * as cohere from "cohere-ai";
+import {
+  LLMRequestTypeValues,
+  SpanAttributes,
+} from "@traceloop/ai-semantic-conventions";
 
 export class CohereInstrumentation extends InstrumentationBase<any> {
   protected override _config!: CohereInstrumentationConfig;
 
   constructor(config: CohereInstrumentationConfig = {}) {
-    super("@traceloop/instrumentation-bedrock", "0.0.17", config);
+    super("@traceloop/instrumentation-cohere", "0.3.0", config);
   }
 
   public override setConfig(config: CohereInstrumentationConfig = {}) {
@@ -44,10 +57,155 @@ export class CohereInstrumentation extends InstrumentationBase<any> {
   }
 
   public manuallyInstrument(module: typeof cohere) {
+    this._wrap(module.CohereClient.prototype, "generate", this.wrapperMethod());
+    this._wrap(
+      module.CohereClient.prototype,
+      "generateStream",
+      this.wrapperMethod(),
+    );
+    this._wrap(module.CohereClient.prototype, "chat", this.wrapperMethod());
+    this._wrap(
+      module.CohereClient.prototype,
+      "chatStream",
+      this.wrapperMethod(),
+    );
+    this._wrap(module.CohereClient.prototype, "rerank", this.wrapperMethod());
+
     return module;
   }
 
-  private wrap() {}
+  private wrap(module: typeof cohere) {
+    this._wrap(module.CohereClient.prototype, "generate", this.wrapperMethod());
+    this._wrap(
+      module.CohereClient.prototype,
+      "generateStream",
+      this.wrapperMethod(),
+    );
+    this._wrap(module.CohereClient.prototype, "chat", this.wrapperMethod());
+    this._wrap(
+      module.CohereClient.prototype,
+      "chatStream",
+      this.wrapperMethod(),
+    );
+    this._wrap(module.CohereClient.prototype, "rerank", this.wrapperMethod());
 
-  private unwrap() {}
+    return module;
+  }
+
+  private unwrap(module: typeof cohere) {
+    // this._unwrap(module.CohereClient.prototype, "generate");
+    this._unwrap(module.CohereClient.prototype, "generateStream");
+    this._unwrap(module.CohereClient.prototype, "chat");
+    this._unwrap(module.CohereClient.prototype, "chatStream");
+    this._unwrap(module.CohereClient.prototype, "rerank");
+  }
+
+  private wrapperMethod(): any {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const plugin = this;
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    return (original: Function) => {
+      return function method(this: any, ...args: any) {
+        const span = plugin._startSpan({
+          params: args[0],
+          methodName:
+            Object.getOwnPropertyDescriptors(original).name.value ?? "",
+        });
+        const execContext = trace.setSpan(context.active(), span);
+        const execPromise = safeExecuteInTheMiddle(
+          () => {
+            return context.with(execContext, () => {
+              return original.apply(this, args);
+            });
+          },
+          () => {},
+        );
+        const wrappedPromise = plugin._wrapPromise(span, execPromise);
+        return context.bind(execContext, wrappedPromise);
+      };
+    };
+  }
+
+  private _wrapPromise<T>(span: Span, promise: Promise<T>): Promise<T> {
+    return promise
+      .then(async (result) => {
+        const awaitedResult = (await result) as
+          | cohere.Cohere.Generation
+          | cohere.Cohere.GenerateStreamedResponse
+          | cohere.Cohere.NonStreamedChatResponse
+          | cohere.Cohere.StreamedChatResponse
+          | cohere.Cohere.RerankResponse;
+
+        this._endSpan({
+          span,
+          result: awaitedResult,
+        });
+
+        return new Promise<T>((resolve) => resolve(result));
+      })
+      .catch((error: Error) => {
+        return new Promise<T>((_, reject) => {
+          span.setStatus({
+            code: SpanStatusCode.ERROR,
+            message: error.message,
+          });
+          span.recordException(error);
+          span.end();
+
+          reject(error);
+        });
+      });
+  }
+
+  private _startSpan({
+    params,
+    methodName,
+  }: {
+    params:
+      | cohere.Cohere.GenerateRequest
+      | cohere.Cohere.GenerateStreamRequest
+      | cohere.Cohere.ChatRequest
+      | cohere.Cohere.ChatStreamRequest;
+    methodName: string;
+  }): Span {
+    const attributes: Attributes = {
+      [SpanAttributes.LLM_VENDOR]: "Cohere",
+      [SpanAttributes.LLM_REQUEST_TYPE]:
+        this._getLlmRequestTypeByMethod(methodName),
+    };
+
+    return this.tracer.startSpan(`cohere.${methodName}`, {
+      kind: SpanKind.CLIENT,
+      attributes,
+    });
+  }
+
+  private _endSpan({
+    span,
+    result,
+  }: {
+    span: Span;
+    result:
+      | cohere.Cohere.Generation
+      | cohere.Cohere.GenerateStreamedResponse
+      | cohere.Cohere.NonStreamedChatResponse
+      | cohere.Cohere.StreamedChatResponse
+      | cohere.Cohere.RerankResponse;
+  }) {
+    span.setStatus({ code: SpanStatusCode.OK });
+    span.end();
+  }
+
+  private _getLlmRequestTypeByMethod(methodName: string) {
+    if (methodName === "chat") return LLMRequestTypeValues.CHAT;
+    else if (methodName === "generate") return LLMRequestTypeValues.COMPLETION;
+    else if (methodName === "rerank") return LLMRequestTypeValues.RERANK;
+    else return LLMRequestTypeValues.UNKNOWN;
+  }
+
+  private _shouldSendPrompts() {
+    return this._config.traceContent !== undefined
+      ? this._config.traceContent
+      : true;
+  }
 }
