b91307be92aab04fdeeb195e0bd9db8cd4731b9a
Nir Gazit <nirga@users.noreply.github.com>
2024-03-27 00:52:32 +0200

    fix(sdk): manually report VectorDB spans

--- packages/traceloop-sdk/src/lib/node-server-sdk.ts
+++ packages/traceloop-sdk/src/lib/node-server-sdk.ts
@@ -5,6 +5,7 @@ export { InitializeOptions } from "./interfaces";
 export { initialize } from "./configuration";
 export { forceFlush } from "./tracing";
 export * from "./tracing/decorators";
+export * from "./tracing/manual";
 export * from "./tracing/association";
 export * from "./tracing/score";
 export * from "./prompts";
