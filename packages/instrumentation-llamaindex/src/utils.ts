fbc8847461b35f12d29f7fbff03f695e7c49b1bd
Gal Kleinman <gal.klm@gmail.com>
2024-12-13 00:16:28 +0200

    missing llamaindex test recordings

--- packages/instrumentation-llamaindex/src/utils.ts
+++ packages/instrumentation-llamaindex/src/utils.ts
@@ -80,7 +80,6 @@ export function genericWrapper(
   tracer: () => Tracer,
   shouldSendPrompts: boolean,
 ) {
-  console.log("genericWrapper", className, methodName, kind, shouldSendPrompts);
   // eslint-disable-next-line @typescript-eslint/ban-types
   return (original: Function) => {
     return function method(this: any, ...args: unknown[]) {
