7fcbc8f7f17e6380135f66916a4d0268c1be3a65
nina-kollman <59646487+nina-kollman@users.noreply.github.com>
2026-01-18 13:15:17 +0200

    no meed

--- packages/traceloop-sdk/rollup.config.js
+++ packages/traceloop-sdk/rollup.config.js
@@ -7,7 +7,7 @@ const name = require("./package.json").main.replace(/\.js$/, "");
 const bundle = (config) => ({
   ...config,
   input: "src/index.ts",
-  external: (id) => !/^[./]/.test(id) && !id.endsWith(".json"),
+  external: (id) => !/^[./]/.test(id),
 });
 
 exports.default = [
